bc9c05589fab84e340e65d5d4a28a4620eec6916
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-16 17:00:12 +0100

    end first snack

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const biciDaCorsa = [
-    { nome: "romana", peso: 5 },
-    { nome: "napoletana", peso: 10 },
-    { nome: "scapece", peso: 15 },
+    { nome: "Rossa", peso: 5 },
+    { nome: "Blu", peso: 10 },
+    { nome: "Verde", peso: 15 },
 ]
 
+let biciLeggera = biciDaCorsa[0]
+
+for (const bici of biciDaCorsa) {
+    if (bici.peso < biciLeggera.peso) {
+        biciLeggera = bici
+    }
+}
+
+console.log(`La bici da corsa più leggera è ${biciLeggera.nome}`)
