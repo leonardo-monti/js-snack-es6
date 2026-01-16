04535aa60023a75beb4c947fdee54f11f6a8366e
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-16 17:39:55 +0100

    end snack 2

--- snack2.js
+++ snack2.js
@@ -1,5 +1,16 @@
 const squadre = [
-    { nome: "Roma", punti: 0, falli: 0 },
-    { nome: "Napoli", punti: 0, falli: 0 },
-    { nome: "Milan", punti: 0, falli: 0 },
+    { nome: "Roma", punti: 0, falliSubiti: 0 },
+    { nome: "Napoli", punti: 0, falliSubiti: 0 },
+    { nome: "Milan", punti: 0, falliSubiti: 0 },
 ]
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+for (const squadra of squadre) {
+    squadra.punti = randomNumber(0, 6)
+    squadra.falliSubiti = randomNumber(0, 4)
+}
+
+console.table(squadre)
